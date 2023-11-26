1e0c3af59885489474f1dca44bddeccee5c04dc7
Kuldeep Singh Rajpoot <kuldeepsinghrajpoot4@gmail.com>
2023-11-26 19:25:32 +0530

    middleware update

--- middleware.js
+++ middleware.js
@@ -1,25 +1,11 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-
-export default withAuth(
-  // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
-    // console.log("path name ->",req.nextUrl.pathname);
-    if (
-      req.nextUrl.pathname === "/user" &&
-      req.nextauth.token?.role !== "user"
-    ) {
-      return new NextResponse("You are not authorized!");
-    }
-  },
-  {
-    callbacks: {
-      authorized: (params) => {
-        let { token } = params;
-        return !!token;
-      },
-    },
-  }
-);
-
-export const config = { matcher: ["/page/:path*", "/doctor/:path*","/user/:path*"] };
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  return NextResponse.redirect(new URL('/page', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/page/:path*',
+}
