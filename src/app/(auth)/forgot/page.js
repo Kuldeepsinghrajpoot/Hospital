4baea9ba208e8193a8b91b8f56579c99401b8b14
Kuldeep Singh Rajpoot <kuldeepsinghrajpoot4@gmail.com>
2023-12-23 21:51:00 +0530

    update tailwind css

--- src/app/(auth)/forgot/page.js
+++ src/app/(auth)/forgot/page.js
@@ -119,7 +119,7 @@ const Forgot = () => {
                     />
                   </div>
                   <button
-                    className="btn btn-primary  w-100"
+                    className="bg-[#7367F0] hover:bg-[#7b70fa] text-white font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded w-full"
                     type="submit"
                     disabled={loading} // Disable the button when in the loading state
                   >
